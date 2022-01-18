be869ff35cebedd1f6c078a42a50cc1089fd862d
Kingofath <89654438+Kingofath@users.noreply.github.com>
2022-01-18 12:20:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+import random
+number = 0
+index = 0
+B = 570315677726089
+def Enter(password):
+    if int(password)*int(password) == B:
+        print("granted")
+inp = input("guess or attack")
+if inp == "guess":
+    guess = input("whats your 4 digit guess")
+    Enter(guess)
+if inp == "attack":
+    while number != B:
+        number = index * index
+        if number != B:
+            index += 1
+        if number == B:
+            print(index)
+            Enter(index)
+
+
